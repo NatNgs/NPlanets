6b927e8b45a2780f6e8ac0219a92cc1693c3c619
Nathael Nogues <nogues.nathael@etu.unice.fr>
2015-02-22 19:25:02 +0100

    Création d'une énum contenant toutes les constantes fonctionnelles

---
+++ src/core/Constants.java
@@ -0,0 +1,15 @@
+package core;
+
+public enum Constants {
+	PlanetDefaultGenTroupesSpeed(10.0), TroupeDefaultAdaptationTime(10.0);
+
+	private double value;
+
+	private Constants(double value) {
+		this.value = value;
+	}
+
+	public double getValue() {
+		return value;
+	}
+}
