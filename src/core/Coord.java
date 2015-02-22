52943d1365d38ebcf917d5b3908bc56c8b46a750
Nathael Nogues <nogues.nathael@etu.unice.fr>
2015-02-22 21:00:02 +0100

    Position et déplacement des vaisseaux

--- src/core/Coord.java
+++ src/core/Coord.java
@@ -9,7 +9,11 @@ public class Coord {
 	public final static Coord CENTER = new Coord(new double[] { 0, 0, 0 });
 	private final double[] coord;
 
-
+	public Coord(Coord c) {
+		coord = new double[c.coord.length];
+		for (int i = 0; i < c.coord.length; i++)
+			coord[i] = c.coord[i];
+	}
 	public Coord(double[] xyz) {
 		if(xyz.length != 3)
 			throw new RuntimeException(
@@ -42,5 +46,29 @@ public class Coord {
 	public static void main(String[] args) {
 		Coord c = new Coord(new double[] { 32, 42.42, 0 });
 		System.out.println(c);
+	}
+
+	/**
+	 * 
+	 * @param cSrc
+	 *            position will be returned if d is 0.0
+	 * @param cDest
+	 *            position will be returned if d is 1.0
+	 * @param d
+	 *            position percentage from cSrc to cDest in range [0, 1] (0.0 =
+	 *            0%, 1.0 = 100%)
+	 * @return new Coordinate at d% of traject from cSrc to cDest
+	 */
+	public static Coord getMidCoord(Coord cSrc, Coord cDest, double d) {
+		if (d <= 0)
+			return cSrc;
+		if (d >= 1)
+			return cDest;
+
+		Coord res = new Coord(cSrc);
+		for (int i = 0; i < cDest.coord.length; i++)
+			res.coord[i] = res.coord[i] * (1 - d) + cDest.coord[i] * d;
+
+		return res;
 	}
 }
--- src/map/Vaisseau.java
+++ src/map/Vaisseau.java
@@ -14,6 +14,8 @@ public class Vaisseau {
 	private Planet pSrc, pDest;
 	private Coord cSrc, cCurt, cDest;
 	private Troupe t;
+	private double travelTime;
+	private double speed;
 
 	public Vaisseau(Planet source, Planet destination, Troupe t) {
 		pSrc = source;
@@ -22,24 +24,55 @@ public class Vaisseau {
 		cDest = destination.getCoord();
 		cCurt = cSrc;
 		this.t = t;
+		travelTime = 0;
+		speed = Constants.VaisseauDefaultSpeed.getValue()
+				* Math.pow(2, t.getCoef(CoefType.Speed));
 	}
 
+	public Coord getCurrentSourceLocation() {
+		return pSrc.getCoord();
+	}
+	public Coord getSourceLocation() {
+		return cSrc;
+	}
 	public Coord getCurrentLocation() {
 		return cCurt;
 	}
-
 	public Coord getDestinationLocation() {
 		return cDest;
 	}
-
 	public Coord getCurrentDestinationLocation() {
 		return pDest.getCoord();
 	}
 
 	public double getTotalTravelTime() {
-		return cSrc.getDistance(cDest)
-				/ (Constants.VaisseauDefaultSpeed.getValue() * Math.pow(2,
-						t.getCoef(CoefType.Speed)));
+		return cSrc.getDistance(cDest) / speed;
+	}
+
+	public double getCurentTravelTime() {
+		return travelTime;
+	}
+
+	public Troupe getTroupe() {
+		return t;
+	}
+
+	/**
+	 * 
+	 * @return 'true' if travel finished, 'false' otherwise
+	 */
+	public boolean increaseTravelTime() {
+		if (travelTime >= getTotalTravelTime())
+			return true;
+
+		travelTime += speed;
+
+		cCurt = Coord.getMidCoord(cSrc, cDest, travelTime
+				/ getTotalTravelTime());
+
+		if (travelTime >= getTotalTravelTime())
+			return true;
+		return false;
 	}
 
 }
