54ecde377348b6ffc128df0f67b4d8c608a3d995
Nathael Nogues <nogues.nathael@etu.unice.fr>
2015-02-23 14:10:34 +0100

    Création de la classe MessagePlayer

--- src/control/Control.java
+++ src/control/Control.java
@@ -1,6 +1,7 @@
 package control;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 import vue.messages.*;
 import vue.players.APlayer;
@@ -14,11 +15,11 @@ import core.Player;
 public class Control {
 	private static final Control instance = new Control();
 	private HashMap<APlayer, Player> players;
-	private HashMap<APlayer, Boolean> playersReady;
+	private HashSet<APlayer> playersReady;
 
 	private Control() {
 		players = new HashMap<>();
-		playersReady = new HashMap<>();
+		playersReady = new HashSet<>();
 	}
 
 	// PRIVATE access methods //
@@ -36,34 +37,61 @@ public class Control {
 	}
 
 	// Command Methods //
-	private Message playerQuit(APlayer aPlayer, Command c) {
+	private Message playerQuit(APlayer aPlayer, CommandQuit c) {
 		if (!players.containsKey(aPlayer))
 			return new MessageError("Player not in game", c);
-		sendMessageToAll(new MessagePlayerLeave(aPlayer.getName()));
+		sendMessageToAll(new MessagePlayer("quit", aPlayer.getName()));
 		players.remove(aPlayer);
 
 		return new MessageOk(c);
 	}
 
-	private Message playerJoin(APlayer aPlayer, Command c) {
+	private Message playerJoin(APlayer aPlayer, CommandJoin c) {
 		if (players.containsKey(aPlayer))
 			return new MessageError("Player already in game", c);
 
 		players.put(aPlayer, new Player());
-		sendMessageToAll(new MessagePlayerJoin(aPlayer.getName()));
+		sendMessageToAll(new MessagePlayer("join", aPlayer.getName()));
+
+		return new MessageOk(c);
+	}
+
+	private Message playerReady(APlayer aPlayer, CommandReady c) {
+		if (!players.containsKey(aPlayer))
+			return new MessageError("Player not in game", c);
+
+		if (c.isReady()) {
+			if (playersReady.contains(aPlayer))
+				return new MessageError("Player already ready", c);
+			else
+				playersReady.add(aPlayer);
+		} else if (!playersReady.contains(aPlayer))
+			return new MessageError("Player is already not ready", c);
+		else
+			playersReady.remove(aPlayer);
+
+		sendMessageToAll(new MessagePlayer(c.isReady() ? "ready" : "not_ready",
+				aPlayer.getName()));
 
 		return new MessageOk(c);
 	}
 
 	// PUBLIC STATIC access methods //
 	public static Message sendCommand(APlayer aPlayer, Command c) {
-		switch (c.getName()) {
-		case "quit":
-			return instance.playerQuit(aPlayer, c);
-		case "join":
-			return instance.playerJoin(aPlayer, c);
+		try {
+			switch (c.getName()) {
+			case "quit":
+				return instance.playerQuit(aPlayer, (CommandQuit) c);
+			case "join":
+				return instance.playerJoin(aPlayer, (CommandJoin) c);
+			case "ready":
+				return instance.playerReady(aPlayer, (CommandReady) c);
+			}
+		} catch (ClassCastException cce) {
+			return new MessageError("Command '" + c.getName() + "' can't be a "
+					+ c.getClass(), c);
 		}
-		return new MessageError("Command untreatable", c);
+		return new MessageError("Command '" + c.getName() + "' is unknown", c);
 	}
 
 }
--- src/vue/messages/CommandReady.java
+++ src/vue/messages/CommandReady.java
@@ -3,9 +3,14 @@ package vue.messages;
 import vue.players.APlayer;
 
 public class CommandReady extends Command {
+	private boolean ready;
 
-	public CommandReady(APlayer sender) {
+	public CommandReady(APlayer sender, boolean ready) {
 		super("ready", sender);
+	}
+
+	public boolean isReady() {
+		return ready;
 	}
 
 }
